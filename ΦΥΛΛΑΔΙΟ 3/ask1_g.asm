364701cafce8022496f0f96e6ac0d7481bf9a73e
jimmygeorg <82272718+jimmygeorg@users.noreply.github.com>
2023-12-14 15:29:03 +0200

    ask1_g.asm

--- ΦΥΛΛΑΔΙΟ 3/ask1_g.asm
+++ ΦΥΛΛΑΔΙΟ 3/ask1_g.asm
@@ -3,12 +3,16 @@
 
 .data
 str: .space 100
-msg1: .asciiz "Enter your first name"
-msg2: .asciiz "Enter your last name"
+msg: .asciiz "Enter your first and last name"
+
 
 .text
 .globl main
-	main:
+	main:	
+		li $v0, 4
+		la $a0, msg
+		syscall
+
 		li $v0, 8	#ετοιμασου να διαβασεις string
 		li $a1, 100	#φορτωνουμε το μεγιστο μηκος του string
 		la $a0, str	#φορτωνουμε στον a0 τη διευθυνση μνημης οπου θα αποκηθευτει το string
